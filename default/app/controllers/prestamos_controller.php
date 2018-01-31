26f9674a3091fed0d5ee67d91c4497dad283b240
anderson2811 <anderson.andres.guerrero@gmail.com>
2018-01-31 15:11:26 -0400

    fixs

--- default/app/controllers/prestamos_controller.php
+++ default/app/controllers/prestamos_controller.php
@@ -11,10 +11,19 @@ class PrestamosController extends AppController
     	View::template("solicitud");
       $this->servicio = Load::model("servicios")->find_by_id($id);
     	$this->sucursales = Load::model("sucursales")->find();
+      // if(Input::post("g-recaptcha-response")){
+      //$fields = array(
+      // 'secret' => 'mikey',
+      // 'response' => Input::post("g-recaptcha-response"),
+      // 'remoteip' => $_SERVER['REMOTE_ADDR']
+      // );
+      // $response = http_post_fields("https://www.google.com/recaptcha/api/siteverify", $fields)
+      // $response['success'] == TRUE
+      // }
       if (Input::haspost("solicitudes")){
         $solicitudes = new Solicitudes(Input::post("solicitudes"));
         if($solicitudes->save()){
-          Flash::valid("Solicitud registrada!");
+          Flash::valid("Registro exitoso!");
         }else{
           Flash::error("Error al registrar la solicitud");
         }
